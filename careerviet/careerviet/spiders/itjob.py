c131968c2cf8deda87c4009d30194aa677375514
nhat.huyng13@gmail.com <nhat.huyng13@gmail.com>
2026-06-10 15:36:40 +0700

    fix missing data and false data page 1, makeup code, add comment and crawl 5 page

--- careerviet/careerviet/spiders/itjob.py
+++ careerviet/careerviet/spiders/itjob.py
@@ -2,12 +2,14 @@ import scrapy
 
 
 def clean_text(value):
+    """Xóa khoảng trắng thừa và xuống dòng trong chuỗi."""
     if not value:
         return ""
     return " ".join(value.strip().split())
 
 
 def first_text(response, selectors):
+    """Lấy giá trị đầu tiên không rỗng từ danh sách các selector."""
     for selector in selectors:
         value = response.css(selector).get()
         value = clean_text(value)
@@ -17,6 +19,7 @@ def first_text(response, selectors):
 
 
 def unique_list(values):
+    """Lấy danh sách các giá trị duy nhất từ danh sách đầu vào."""
     result = []
     for value in values:
         value = clean_text(value)
@@ -32,29 +35,29 @@ class CareerVietSpider(scrapy.Spider):
         "https://careerviet.vn/viec-lam/cntt-phan-cung-mang-cntt-phan-mem-c63,1-trang-1-vi.html"
     ]
 
-    custom_settings = {
-        "FEED_EXPORT_ENCODING": "utf-8-sig",
-        "DOWNLOAD_DELAY": 1,
-        "CONCURRENT_REQUESTS_PER_DOMAIN": 1,
-    }
-
     def parse(self, response):
-        jobs = response.css("div[class*='job-item']")
+        # Lấy tất cả các phần tử chứa thông tin công việc
+        jobs = response.css("div[class*='job-item']") 
 
+        # Duyệt qua từng phần tử công việc và lấy liên kết chi tiết
         for job in jobs:
             link = job.css(".figcaption .title h2 a::attr(href)").get()
 
             if link:
+                # Nếu link là đường dẫn tương đối, chuyển nó thành đường dẫn tuyệt đối
                 yield response.follow(
                     link,
                     callback=self.parse_job
                 )
 
+        # Lấy số trang hiện tại từ meta, nếu không có thì mặc định là 1
         current_page = response.meta.get("page", 1)
-
-        if current_page < 1:
+        print(f"Crawling page {current_page}...")
+        # Giới hạn số trang cần crawl
+        if current_page < 5:
             next_page = current_page + 1
 
+            # Tạo URL cho trang tiếp theo dựa trên mẫu URL của trang hiện tại
             next_url = (
                 "https://careerviet.vn/viec-lam/"
                 f"cntt-phan-cung-mang-cntt-phan-mem-c63,1-trang-{next_page}-vi.html"
@@ -67,77 +70,79 @@ class CareerVietSpider(scrapy.Spider):
             )
 
     def parse_job(self, response):
+
+        job_id = response.url.split(".")[-2]
+
         job_title = first_text(response, [
-            ".job-desc h1.title::text",
-            "div.title > h2::text",
-            ".job-detail-page h1::text",
-            "h1::text",
+            "div[class*='job'] h1::text",
+            "div[class*='job'] h2::text",
         ])
 
         company = first_text(response, [
-            ".job-desc a::text",
-            ".job-detail-page .caption > a::text",
-            ".job-detail-page div.head-right a::text",
-            ".company-name a::text",
-            "#aboutustp-desktop-not-login .top-job-info div::text",
+            "div[class*='job'] a[class*='company']::text",
+            ".tit_company::text",
         ])
 
         job_location = first_text(response, [
-            "#tab-1 div:nth-child(1) a::text",
-            "#info-career-desktop li.info-workplace a::text",
-            ".right-welfares a::text",
-            ".detail-row.box-welfares .hide-in-mobile a::text",
+            ".place::text",
+            "p.list-workplace a::text",
+            ".map a::text",
         ])
 
         date_posted = first_text(response, [
-            "#tab-1 div:nth-child(2) p::text",
-            "tr:nth-child(6) > td.content > p::text",
+            ".box-info tr:nth-child(6) > td.content p::text",
+            ".tab-content div:nth-child(2) li:nth-child(1) p::text",
+            ".table tr:nth-child(8) td.content p::text",
+            ".table div:nth-child(1) tr:nth-child(3) td.content p::text",
         ])
 
         job_field = unique_list(
-            response.css("#tab-1 div:nth-child(2) li:nth-child(2) a::text").getall()
-            or response.css("#info-career-desktop li:nth-child(5) a::text").getall()
-            or response.css(".job-detail-page tr:nth-child(1) a::text").getall()
-            or response.css("#info-career-desktop li:nth-child(5) a::text").getall()
+            response.css(".tab-content div:nth-child(2) li:nth-child(2) p a::text").getall()
+            or response.css(".box-info tr:nth-child(1) > td.content a::text").getall()
+            or response.css(".table tr:nth-child(1) td.content a::text").getall()
+            or response.css(".info-career li:nth-child(5) a::text").getall()
         )
 
         job_type = first_text(response, [
-            "li:nth-child(3) > p::text",
-            ".job-detail-page tr:nth-child(3) td.content > p::text",
+            ".tab-content div:nth-child(2) li:nth-child(3) p::text",
+            ".box-info tr:nth-child(3) td.content p::text",
+            ".table tr:nth-child(3) td.content p::text",
         ])
 
         salary = first_text(response, [
-            "#tab-1 div:nth-child(3) li:nth-child(1) > p::text",
-            "#info-career-desktop li:nth-child(3) > div::text",
-            ".job-detail-page tr:nth-child(2) td.content strong::text",
+            ".tab-content div:nth-child(3) li:nth-child(1) > p::text",
+            ".box-info tr:nth-child(2) td.content strong::text",
+            ".box-info tr:nth-child(6) td.content p::text",
+            ".info-career li:nth-child(3) div::text",
         ])
 
         experience = first_text(response, [
-            "#tab-1 div:nth-child(3) li:nth-child(2) > p::text",
-            "#info-career-desktop li:nth-child(4) > div::text",
-            ".body-template div div div:nth-child(2) tr:nth-child(2) td.content > p::text",
-            ".body-template div div div:nth-child(2) td.content > p::text",
-            ".full-content > div:nth-child(2) p:nth-child(3)::text",
-            "#jobDetailContent div:nth-child(2) > table > tbody > tr:nth-child(2) > td.content > p::text",
-            ".full-content > div:nth-child(2) > div li:last-child::text",
+            ".template06 .detail-row:nth-child(2) li:last-child::text",
+            ".tab-content div:nth-child(3) li:nth-child(2) > p::text",
+            ".table tr:nth-child(6) td.content p::text",
+            ".box-info div:nth-child(2) > table > tbody > tr:nth-child(2) > td.content > p::text",
+            ".template-201 .detail-row:nth-child(3) li:nth-child(3)::text",
+            ".template-200 .detail-row:nth-child(3) li:nth-child(2)::text",
         ])
 
         pos = first_text(response, [
-            "#tab-1 div:nth-child(3) li:nth-child(3) > p::text",
-            "#info-career-desktop li:nth-child(5) > div::text",
-            ".job-detail-page tr:nth-child(4) td.content > p::text",
-            "#info-career-desktop li:nth-child(2) > div::text",
+            ".box-info tr:nth-child(4) td.content p::text",
+            ".tab-content div:nth-child(3) li:nth-child(3) > p::text",
+            ".box-info div:nth-child(2) > table > tbody > tr:nth-child(1) > td.content > p::text",
+            ".box-info tr:nth-child(5) td.content p::text",
+            ".info-career li:nth-child(2) div::text",
         ])
 
         deadline = first_text(response, [
-            "tr:nth-child(5) > td.content > p::text",
-            "div:nth-child(3) li:nth-child(4) > p::text",
-            "#info-career-desktop li:nth-child(6) > div::text",
-            "#info-career-desktop li:nth-child(4) > div::text",
-            "#tab-1 div:nth-child(3) li:nth-child(3) > p::text",
+            ".box-info tr:nth-child(5) td.content p::text",
+            ".box-info div:nth-child(2) > table > tbody > tr:nth-child(3) > td.content > p::text",
+            ".box-info tr:nth-child(7) td.content p::text",
+            ".tab-content div:nth-child(3) li:nth-child(4) > p::text",
+            ".info-career li:nth-child(4) div::text",
         ])
-
+    
         yield {
+            "job_id": job_id,
             "job_title": job_title,
             "company": company,
             "job_location": job_location,
